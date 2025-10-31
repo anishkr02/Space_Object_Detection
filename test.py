ef76ec8aae7e36cf24cbc178e67ca45993df49b9
Anish Sah <kumaranish03532@gmail.com>
2025-10-31 11:00:14 +0530

    Change model and video paths to relative
    Updated model and video paths to relative paths.

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import cv2
 from ultralytics import YOLO
 
 # Load your trained model
-model = YOLO("C:\\Users\\kumar\\Desktop\\NASA Spaceapp\\NASA_fnf_model\\detect_best.pt")   # path to your trained weights
+model = YOLO("detect_best.pt")   # path to your trained weights
 
 # Function for video detection
 def detect_from_video(video_path):
@@ -59,7 +59,7 @@ def detect_from_webcam():
 if __name__ == "__main__":
     mode = input("Enter 'v' for video or 'w' for webcam: ").strip().lower()
     if mode == 'v':
-        detect_from_video("C:\\Users\\kumar\\Desktop\\NASA Spaceapp\\NASA_fnf_model\\Video2.mp4")
+        detect_from_video("Video2.mp4")
     elif mode == 'w':
         detect_from_webcam()
     else:
